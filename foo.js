c310a676ed49d6a09bd030b6157c7694971433c4
Sebastiaan Deckers <seb@ninja.sg>
2016-01-28 16:56:33 +0800

    Playing around with ES6 module tracing

---
+++ foo.js
@@ -0,0 +1,1 @@
+console.log('now inside foo')
--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+import path from 'path'
 import fs from 'fs'
 import http2 from 'http2'
+
+// import { System } from 'es6-module-loader'
+// System.transpiler = 'babel'
+// System.trace = true
+// System.execute = false
+// console.log(Loader, System)
+// System
+//   .import(path.join(__dirname, 'foo.js'))
+//   .import('./foo.js')
+//   .then(function () {
+//     console.log('done loading foo.js')
+//     console.log(System.loads['./foo.js'])
+//   })
 
 const options = {
   key: fs.readFileSync('./key.pem'),
@@ -7,8 +21,19 @@ const options = {
 }
 
 const server = http2.createServer(options, (request, response) => {
-  console.log('got incoming')
+  console.log('got incoming', request.url)
+  if (response.push) {
+    console.log('supports push')
+    var push = response.push('/stuff.txt')
+    var stream = fs.createReadStream(path.join(__dirname, '/stuff.txt'))
+    push.writeHead(200)
+    console.log('pushing stuff.txt')
+    stream.pipe(push)
+  } else {
+    console.log('no push support')
+  }
   response.end('Hello world!')
+  // setTimeout(() => response.end('Hello world!'), 5000)
 })
 server.listen(8080)
 
